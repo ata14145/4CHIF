a0fd28496f25b9a8150f2263fe5571fb5f3630f6
Mohamed <mohamed_94@live.at>
2013-11-24 15:34:48 +0100

    Initial Comment.

--- JDBC 1/src/JDBCexample.java
+++ JDBC 1/src/JDBCexample.java
@@ -2,95 +2,97 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
- 
+
 public class JDBCexample {
- 
+
 	private static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
 	private static final String DB_CONNECTION = "jdbc:oracle:thin:@localhost/";
 	private static final String DB_USER = "user";
 	private static final String DB_PASSWORD = "password";
- 
+
 	public static void main(String[] argv) {
- 
+
 		try {
- 			insertToTable();
- 		} catch (SQLException e) {
- 			System.out.println(e.getMessage());
- 		}
- 	}
- 
+			insertToTable();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
 	private static void insertToTable() throws SQLException {
- 
+
 		Connection dbConnection = null;
 		PreparedStatement preparedStatement = null;
- 
+
 		String insertTableSQL = "INSERT INTO DBUSER"
 				+ "(USER_ID, USERNAME, CREATED_BY, CREATED_DATE) VALUES"
 				+ "(?,?,?,?)";
- 
+
 		try {
 			dbConnection = getDBConnection();
 			preparedStatement = dbConnection.prepareStatement(insertTableSQL);
- 
+
 			dbConnection.setAutoCommit(false);
- 
+
 			preparedStatement.setInt(1, 101);
 			preparedStatement.setString(2, "ata1");
 			preparedStatement.setString(3, "schule");
 			preparedStatement.setTimestamp(4, getCurrentTimeStamp());
 			preparedStatement.addBatch();
- 
+
 			preparedStatement.setInt(1, 102);
 			preparedStatement.setString(2, "ata2");
 			preparedStatement.setString(3, "schule");
 			preparedStatement.setTimestamp(4, getCurrentTimeStamp());
 			preparedStatement.addBatch();
- 
+
 			preparedStatement.setInt(1, 103);
 			preparedStatement.setString(2, "ata3");
 			preparedStatement.setString(3, "schule");
 			preparedStatement.setTimestamp(4, getCurrentTimeStamp());
 			preparedStatement.addBatch();
- 
+
 			preparedStatement.executeBatch();
- 
+
 			dbConnection.commit();
- 
+
 			System.out.println("Record is inserted into DBUSER table!");
- 
+
 		} catch (SQLException e) {
- 
+
 			System.out.println(e.getMessage());
 			dbConnection.rollback();
- 
+
 		} finally {
- 			if (preparedStatement != null) {
+			if (preparedStatement != null) {
 				preparedStatement.close();
 			}
- 			if (dbConnection != null) {
+			if (dbConnection != null) {
 				dbConnection.close();
 			}
- 		}
- 	}
- 	private static Connection getDBConnection() {
- 		Connection dbConnection = null;
- 		try {
- 			Class.forName(DB_DRIVER);
- 		} catch (ClassNotFoundException e) {
- 			System.out.println(e.getMessage());
- 		}
- 		try {
- 			dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER,DB_PASSWORD);
+		}
+	}
+
+	private static Connection getDBConnection() {
+		Connection dbConnection = null;
+		try {
+			Class.forName(DB_DRIVER);
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER,
+					DB_PASSWORD);
 			return dbConnection;
- 		} catch (SQLException e) {
- 			System.out.println(e.getMessage());
- 		}
- 		return dbConnection;
- 	}
- 
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return dbConnection;
+	}
+
 	private static java.sql.Timestamp getCurrentTimeStamp() {
- 		java.util.Date today = new java.util.Date();
+		java.util.Date today = new java.util.Date();
 		return new java.sql.Timestamp(today.getTime());
- 	}
- 
+	}
+
 }
